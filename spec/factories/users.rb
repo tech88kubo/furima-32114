88cda1521431b5c5d3a9caf5d36c3eb0a2ec197f
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-08 19:19:19 +0900

    FactoryBotの値を編集

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname           {"test"}
-    firsrt_name        {"太郎"}
-    last_name          {"田中"}
-    first_kana         {"タロウ"}
-    last_kana          {"タナカ"}
-    email              {"test@example"}
-    encrypted_password {123abc}
+    nickname              {"test"}
+    email                 {"test@example"}
+    password              {"123abc"}
+    password_confirmation {"123abc"}
+    first_name            {"太郎"}
+    last_name             {"田中"}
+    first_kana            {"タロウ"}
+    last_kana             {"タナカ"}
+    birthday              {"2000-01-01"} #モデルのテストの際、Sequel Proでどのような形でデータが保存されているのかを確認する
   end
 end
