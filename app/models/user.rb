0c13404ca13669745a5e4bf6ca74d164aef2e7c6
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-09 15:53:05 +0900

    バリデーションにノマ点の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
 
   with_options presence:true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name, format:  { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}
+    validates :last_name, format:  { with: /\A[ぁ-んァ-ン一-龥々]/, message: "is invalid. Input full-width characters."}
     validates :first_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :last_kana, format:  { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :birthday
