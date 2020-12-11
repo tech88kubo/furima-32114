2e776e38683a8a5e09c5c1c419592ca5a0610466
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-11 19:29:03 +0900

    itemsコントローラcreateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,16 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :shipping_charges_id, :days_to_ship_id, :prefectures_id, :price, :image).merge(user_id: current_user.id)
   end
 end
