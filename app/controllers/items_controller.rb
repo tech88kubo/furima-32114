2144181901de83524b7ec01257003abf726cd7a7
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-15 11:46:12 +0900

    item_controller.rbの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    unless current_user.id == @item.user_id
       redirect_to action: :index
     end
   end
