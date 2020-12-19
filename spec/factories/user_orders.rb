3016479a3dccf61cca93fc8fa5b0f284ec255d45
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-19 18:57:01 +0900

    Formオブジェクト用のモデル単体テスト

---
+++ spec/factories/user_orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user_order do
+    postal_code { "123-4567" }
+    phone_number { "09012345678" }
+    prefectures_id { 14 }
+    municipality { "横浜市緑区" }
+    address { "青山1-1-1" }
+    building_name { "柳ビル" }
+  end
+end
