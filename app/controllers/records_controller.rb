1659cd93e7bdd99c03ab11c8fae3284ceb71a1b0
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-19 18:54:46 +0900

    購入記録をDBに保存する

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,11 +1,29 @@
 class RecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @user_order = UserOrder.new
   end
 
   def create
-    binding.pry
+    @user_order = UserOrder.new(order_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:user_order).permit(:postal_code, :phone_number, :prefectures_id, :municipality, :address, :building_name).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,17 +1,17 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :phone_number, :prefectures_id, :municipality, :address, :building_name, :record, :user, :item
+  attr_accessor :postal_code, :phone_number, :prefectures_id, :municipality, :address, :building_name, :user_id, :item_id
 
   with_options presence: true do
-    validates :postal_code
-    validates :phone_number
-    validates :prefectures_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}
+    validates :prefectures_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
   end
 
   def save
-    Address.create(postal_code: postal_code, phone_number: phone_number, prefectures_id: prefectures_id, municipality: municipality, address: address building_name: building_name, record: record)
-    Record.create(user: user, item: item)
+    record = Record.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, phone_number: phone_number, prefectures_id: prefectures_id, municipality: municipality, address: address, building_name: building_name, record_id: record.id)
   end
 end
