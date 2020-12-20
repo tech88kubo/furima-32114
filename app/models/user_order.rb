97d811a0c1ca2e4c5690f676c00e3f253df1385f
tech88kubo <a_miss_oita@yahoo.co.jp>
2020-12-20 17:42:23 +0900

    tokenのバリデーションを記述

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,6 +1,6 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor :postal_code, :phone_number, :prefectures_id, :municipality, :address, :building_name, :user_id, :item_id
+  attr_accessor :postal_code, :phone_number, :prefectures_id, :municipality, :address, :building_name, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,6 +8,7 @@ class UserOrder
     validates :prefectures_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
+    validates :token
   end
 
   def save
